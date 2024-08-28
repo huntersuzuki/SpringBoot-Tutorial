c82566d635a4a2fb830886bdc45665a2fed22beb
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-08-28 18:17:41 +0530

    Main application file

--- htmlrendering/src/main/java/com/pranay/htmlrendering/HtmlrenderingApplication.java
+++ htmlrendering/src/main/java/com/pranay/htmlrendering/HtmlrenderingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HtmlrenderingApplication {
+public class HtmlrenderingApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HtmlrenderingApplication.class, args);
