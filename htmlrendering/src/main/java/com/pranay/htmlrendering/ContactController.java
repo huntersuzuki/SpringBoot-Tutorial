9a8c343509c581c3d2386b53d56fcf3d1973a7a2
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-08-30 13:58:45 +0530

    used new annotations and getter and setter method to retrive form data

--- htmlrendering/src/main/java/com/pranay/htmlrendering/ContactController.java
+++ htmlrendering/src/main/java/com/pranay/htmlrendering/ContactController.java
@@ -3,11 +3,16 @@ package com.pranay.htmlrendering;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.pranay.htmlrendering.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 @Controller
 public class ContactController {
     @GetMapping("/index")
-    public String IndexDisplay(){
+    public String IndexDisplay() {
         return "contact/index";
     }
 
@@ -16,5 +21,15 @@ public class ContactController {
         m.addAttribute("contact", new Contact());
         return "contact/contact";
     }
-    
+
+    @PostMapping("/contact")
+    public String getDetails(@ModelAttribute Contact contact,Model m) {
+        // System.out.println("In POST mapping handler method");
+        System.out.println("Name is: "+contact.getName());
+        System.out.println("Email is: "+contact.getEmail());
+        System.out.println("Mobile is: "+contact.getMobile());
+        m.addAttribute("contact", contact);
+        return "contact/index";
+    }
+
 }
