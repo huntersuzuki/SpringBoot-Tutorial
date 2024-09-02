b7f8eb73fe52b09fbc9c411f4c24ac6c3a717826
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-09-02 18:55:14 +0530

    Updated file, form data accessing and displaying

--- hello_one/src/main/java/com/greet/hello_one/HomeController.java
+++ hello_one/src/main/java/com/greet/hello_one/HomeController.java
@@ -2,10 +2,8 @@ package com.greet.hello_one;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Controller
--- student/src/main/java/com/pranay/student/EmployeeController.java
+++ student/src/main/java/com/pranay/student/EmployeeController.java
@@ -3,6 +3,10 @@ package com.pranay.student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class EmployeeController {
@@ -16,5 +20,11 @@ public class EmployeeController {
         m.addAttribute("employee", new Employee());
         return "employee";
     }
-
+    @PostMapping("/employee")
+    public String EmployeePost(@ModelAttribute Employee emp,Model m) {
+        System.out.println("FirstName is "+emp.getFirstName());
+       m.addAttribute("employee", emp);
+        return "index";
+    }
+    
 }
